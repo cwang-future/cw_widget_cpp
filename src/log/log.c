694719f5513eb72de350637825e20028757ab965
cwang-future <cwang_future@163.com>
2024-06-25 14:07:44 +0800

    更新日志库

--- src/log/log.c
+++ src/log/log.c
@@ -3,12 +3,12 @@
 
 struct LogContex
 {
-	char filename[128];     /*文件名字*/
-	char filenameold[136];     /*备份文件名字*/
-	FILE *logfilefp;    /*写入文件的文件指针*/
-	int level;  /*日志层级*/
-	bool quiet; /*是否在控制台打印 true 否 false 是*/
-	unsigned int filetotalsize; /*日志文件的大小*/
+	char filename[128];
+	char filenameold[136];
+	FILE *logfilefp;
+	int level;
+	bool quiet;
+	unsigned int filetotalsize;
 };
 static struct LogContex L = {"", "", NULL, 1, false, 32 * 1024};
 
@@ -17,7 +17,8 @@ static const char *level_strings[] =
 	"DEBUG", "INFO", "WARN", "ERROR"
 };
 
-// static const char *level_colors[] = {
+// static const char *level_colors[] = 
+// {
 //   "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m"
 // };
 
@@ -25,8 +26,7 @@ static void print_log(log_Event *ev)
 {
 	int i = 0;
 	const char *p = ev->file, *p1 = NULL;
-	/*截断需要打印的文件路径*/
-	while ((p1 = strstr(p, "/")) != NULL)
+	while ((p != NULL) && ((p1 = strstr(p, "/")) != NULL))
 	{
 		p1++;
 		if (p1 && (strstr(p1, "/") == NULL))
@@ -42,21 +42,24 @@ static void print_log(log_Event *ev)
 		/*线程号版*/
 		// fprintf(ev->fp, "%s [%lu][%-5s] %s:%d : ", ev->buftime,  (unsigned long int)pthread_self(), level_strings[ev->level], p, ev->line);
 		/*颜色版*/
-		/*fprintf(ev->fp, "%s \x1b[94m[%lu]%s[%-5s] \x1b[90m%s:%d:\x1b[0m ",
-				ev->buftime, (unsigned long int)pthread_self(), level_colors[ev->level], level_strings[ev->level], p, ev->line);*/
-		
+		// if (ev->fp == L.logfilefp)
+		// {
+		// 	fprintf(ev->fp, "%s [%-5s] %s:%d : ", ev->buftime, level_strings[ev->level], p, ev->line);
+		// }
+		// else
+		// {
+		// 	fprintf(ev->fp, "%s \x1b[94m[%lu]%s[%-5s] \x1b[90m%s:%d:\x1b[0m ",
+		// 		ev->buftime, (unsigned long int)pthread_self(), level_colors[ev->level], level_strings[ev->level], p, ev->line);
+		// }
 	}
 	else
 	{
 		fprintf(ev->fp, "%s [%-5s] %s:%d : ", ev->buftime, level_strings[ev->level], ev->file , ev->line);
 	}
-	/*格式化字符串并存储到动态分配的字符串中*/
 	vfprintf(ev->fp, ev->fmt, ev->ap);
 	fflush(ev->fp);
 }
 
-
-/*备份日志文件*/
 static void log_file_backup()
 {
 	int ret = 0;
@@ -71,11 +74,9 @@ static void log_file_backup()
 	rename(L.filename, L.filenameold);
 }
 
-/*日志属性设置
-filename : 文件名字，可为NULL 
-level : 设置日志层级 大于等于level的日志等级都打印 即小于level的日志等级不打印
-quiet : 为 true 不打印在控制台； 为 false 打印在控制台
-filetotalsize ：日志文件大小， 超过这个值将重置文件(备份现在日志文件为xx.log.old, 再次创建xx.log文件开始写日志)，当filename参数为NULL时，该值无用，可为0
+/*
+例： log_attribute_set("./templog.log", 0, false, 32*1024);
+	或 log_attribute_set(NULL, 0, false, 0);
 */
 void log_attribute_set(const char *filename, int level, bool quiet, unsigned int filetotalsize)
 {
@@ -89,7 +90,7 @@ void log_attribute_set(const char *filename, int level, bool quiet, unsigned int
 			L.logfilefp = fp;
 		}
 	}
-	if (level >= DEBUG && level <= ERROR)
+	if (level >= LOG__DEBUG && level <= LOG__ERROR)
 	{
 		L.level = level;
 	}
@@ -99,7 +100,7 @@ void log_attribute_set(const char *filename, int level, bool quiet, unsigned int
 		L.filetotalsize = filetotalsize;
 	}
 }
-/*获取时间*/
+
 static void gettime_(char* strtime, int strtime_size)
 {
 	struct tm tm;
@@ -130,9 +131,9 @@ void log_log(int level, const char *file, int line, const char *fmt, ...)
 	if (!L.quiet && level >= L.level) 
 	{
 		ev.fp = stderr;
-		va_start(ev.ap, fmt);   /* 初始化可变参数列表*/
+		va_start(ev.ap, fmt);
 		print_log(&ev);
-		va_end(ev.ap);  /* 结束可变参数列表*/
+		va_end(ev.ap);
 	}
 	if (!L.quiet && L.logfilefp && level >= L.level)
 	{
@@ -143,9 +144,9 @@ void log_log(int level, const char *file, int line, const char *fmt, ...)
 			L.logfilefp = fopen(L.filename, "a+");
 		}
 		ev.fp = L.logfilefp;
-		va_start(ev.ap, fmt);   /* 初始化可变参数列表*/
+		va_start(ev.ap, fmt);
 		print_log(&ev);
-		va_end(ev.ap);  /* 结束可变参数列表*/
+		va_end(ev.ap);
 	}
 }
 
--- src/log/log.h
+++ src/log/log.h
@@ -10,10 +10,9 @@
 #include<unistd.h>
 #include <sys/stat.h>
 /*若需要添加线程号打印，编译时请添加pthread动态库*/
-// #include <pthread.h>
+#include <pthread.h>
 
 #define LOG_VERSION "1.0"
-
 
 typedef struct 
 {
@@ -27,17 +26,13 @@ typedef struct
 	bool color;
 } log_Event;
 
+enum {LOG__DEBUG, LOG__INFO, LOG__WARN, LOG__ERROR};
 
+#define LOG_DEBUG(...) log_log(LOG__DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...)  log_log(LOG__INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...)  log_log(LOG__WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) log_log(LOG__ERROR, __FILE__, __LINE__, __VA_ARGS__)
 
-enum { DEBUG, INFO, WARN, ERROR };
-
-
-#define LOG_DEBUG(...) log_log(DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_INFO(...)  log_log(INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_WARN(...)  log_log(WARN,  __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_ERROR(...) log_log(ERROR, __FILE__, __LINE__, __VA_ARGS__)
-
-/*日志文件一些属性设置*/
 void log_attribute_set(const char* filename, int level, bool quiet, unsigned int filetotalsize);
 
 void log_log(int level, const char *file, int line, const char *fmt, ...);
